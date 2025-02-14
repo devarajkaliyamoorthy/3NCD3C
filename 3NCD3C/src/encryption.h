e90a6eccb96e67e9ace7394750fb65e16f1bcb13
Devaraj Kaliyamoorthy <devarajkaliyamoorthy@gmail.com>
2025-02-14 16:57:18 +0530

    Add files via upload

---
+++ 3NCD3C/src/encryption.h
@@ -0,0 +1,9 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+#include <string>
+
+// Function to encrypt text using Caesar Cipher
+std::string encrypt(const std::string& text, int shift);
+
+#endif
